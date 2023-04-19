2f60727696d9eaef7d9bf406707cb61c3ad56155
Woo-Yeol <king199276@gmail.com>
2023-04-19 02:11:14 +0000

    model : Modify XGBoost model

--- src/models/GBM/XGBoost_model.py
+++ src/models/GBM/XGBoost_model.py
@@ -1,5 +1,6 @@
 import xgboost as xgb
 from wandb.xgboost import WandbCallback
+from sklearn.preprocessing import LabelEncoder
 
 
 class XGBoost():
@@ -10,9 +11,10 @@ class XGBoost():
             "tree_method": "gpu_hist",
             }
         self.data = data
-        self.dataloader(data)
         self.seed = args.seed
         self.num_boost_round = args.num_boost_round
+        self.reg = args.reg
+        self.evals = [(data['X_valid'], data['y_valid'])]
 
     def dataloader(self,data):
         self.dtrain_reg = xgb.DMatrix(data['X_train'],data['y_train'], enable_categorical=True)
@@ -20,17 +22,39 @@ class XGBoost():
         self.evals = [(self.dvalid_reg, 'validation')]
 
     def train(self):
-        self.model = xgb.train(
-            params=self.params,
-            dtrain = self.dtrain_reg,
-            num_boost_round = self.num_boost_round,
-            evals=self.evals,
-            verbose_eval = 1000,
-            early_stopping_rounds = 50,
-            callbacks=[WandbCallback(log_model=True)]
-            )
-        
+        if self.reg:
+            self.dataloader(self.data)
+            self.model = xgb.train(
+                params=self.params,
+                dtrain = self.dtrain_reg,
+                num_boost_round = self.num_boost_round,
+                # eval_metric='rmse',
+                evals=self.evals,
+                verbose_eval = 1000,
+                early_stopping_rounds = 50,
+                callbacks=[WandbCallback(log_model=True)]
+                )
+        # else:
+        #     self.model = xgb.XGBClassifier(
+        #         num_boost_round = self.num_boost_round,
+        #         eval_metric='rmse',
+        #         evals=self.evals,
+        #         verbose_eval = 1000,
+        #         early_stopping_rounds = 50,
+        #         callbacks=[WandbCallback(log_model=True)],
+        #         # n_estimators=1000,
+        #         # learning_rate=0.01, 
+        #         max_depth=10, 
+        #         random_state = self.seed,
+        #         tree_method="gpu_hist",
+        #         ).fit(
+        #         X=self.data['X_train'], 
+        #         y=self.data['y_train'],)
     def pred(self, test):
-        self.dtest_reg = xgb.DMatrix(test, enable_categorical=True)
-        predict = self.model.predict(self.dtest_reg)
+        if self.reg:
+            self.dtest_reg = xgb.DMatrix(test, enable_categorical=True)
+            predict = self.model.predict(self.dtest_reg)
+        # else:
+        #     predict = self.model.predict(test)
+        #     sample = self.model.predict_proba(test)
         return predict
